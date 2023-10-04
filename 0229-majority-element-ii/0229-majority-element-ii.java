7a935e3877d23b4cf857f7fce6c5b7f519a54087
Ridhi Singla <78259552+Ridhi-singla001@users.noreply.github.com>
2023-10-04 10:27:24 +0530

    Time: 2 ms (98.53%), Space: 46.7 MB (47.39%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,18 +1,43 @@
 class Solution {
     public List<Integer> majorityElement(int[] arr) {
         int n=arr.length;
-        List<Integer> ans=new ArrayList<>();
-        HashMap<Integer,Integer>mp=new HashMap<>();
+
+        List<Integer>ans=new ArrayList<>();
+        int el1=0;
+        int el2=0;
+        int cnt1=0;
+        int cnt2=0;
 
         for(int i=0;i<n;i++){
-            int val=mp.getOrDefault(arr[i],0);
-            mp.put(arr[i],val+1);
-        }
-        for (int key : mp.keySet()) {
-            if (mp.get(key) > n / 3) {
-                ans.add(key);
+            if(cnt1==0 && arr[i]!=el2){
+                el1=arr[i];
+                cnt1=1;
+            }else if(cnt2==0 && arr[i]!=el1){
+                el2=arr[i];
+                cnt2=1;
+            }else if(el1==arr[i]){
+                cnt1++;
+            }else if(el2==arr[i]){
+                cnt2++;
+            }else{
+                cnt1--;
+                cnt2--;
             }
         }
+
+           cnt1=0;
+           cnt2=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==el1){
+                cnt1++;
+            }else if(arr[i]==el2){
+                cnt2++;
+            }
+        }
+
+        if(cnt1>n/3) ans.add(el1);
+        if(cnt2>n/3) ans.add(el2);
+
         return ans;
     }
 }
